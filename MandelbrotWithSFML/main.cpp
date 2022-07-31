466ead183fc12369848b5301bf56ecbb57b76617
Chaustere <besnard.owen@gmail.com>
2022-07-31 02:02:41 +0200

    Resizing the window is now possible; fixed some graphical issues

--- MandelbrotWithSFML/main.cpp
+++ MandelbrotWithSFML/main.cpp
@@ -28,12 +28,13 @@ int main()
 
 	// The following line registers all fullscreen modes are available for this computer
 	std::vector<sf::VideoMode> modes = sf::VideoMode::getFullscreenModes();
+	sf::VideoMode deskMode = sf::VideoMode::getDesktopMode();
 
 	// Using the following method make the window be in fullscreen mode at the start of the program
 	//sf::RenderWindow window(modes[0], "Conway's Game of Life simulation", sf::Style::Fullscreen);
 	sf::ContextSettings settings(0U, 0U, 16);
 	// Initializes the window with a width of 512 pixels and a height of 256 pixels
-	sf::RenderWindow window(modes[0], "Mandelbrot Set", sf::Style::Fullscreen, settings);
+	sf::RenderWindow window(deskMode, "Mandelbrot Set", sf::Style::Default, settings);
 	
 	// Sets the framerate limit of the window
 	window.setFramerateLimit(60);
@@ -43,8 +44,9 @@ int main()
 	// Variables declarations //
 
 	sf::VertexArray cList(sf::PrimitiveType::Points, 0);
-	int x_acc = 1920; // X Accuracy
-	int y_acc = 1080; // Y Accuracy
+	float accFactor = 1.1;
+	int x_acc = deskMode.width * accFactor; // X Accuracy
+	int y_acc = deskMode.height * accFactor; // Y Accuracy
 
 	bool hasCalculated = false;
 
@@ -54,7 +56,8 @@ int main()
 
 	// Rectangle around cursor //
 
-	sf::RectangleShape zoomRect(sf::Vector2f(500, 500 * window.getSize().y / window.getSize().x));
+	int zoomRectBaseWidth = window.getSize().x / 10;
+	sf::RectangleShape zoomRect(sf::Vector2f(zoomRectBaseWidth, zoomRectBaseWidth * window.getSize().y / window.getSize().x));
 	zoomRect.setPosition(sf::Vector2f(0, 0));
 	zoomRect.setFillColor(sf::Color::Transparent);
 	zoomRect.setOutlineThickness(2);
@@ -84,24 +87,41 @@ int main()
 						zoomRect.setScale(0.1, 0.1);
 					break;
 
+				case sf::Event::Resized:
+				{
+					sf::FloatRect visibleArea(0, 0, event.size.width, event.size.height);
+					window.setView(sf::View(visibleArea));  // Unstretching the figure
+
+					x_acc = event.size.width * accFactor;  // Resizing the pixels number in the vertex array
+					y_acc = event.size.height * accFactor;
+
+					zoomRectBaseWidth = event.size.width / 10;
+					zoomRect.setSize(sf::Vector2f(zoomRectBaseWidth, zoomRectBaseWidth * event.size.height / event.size.width));
+					zoomRect.setOrigin(sf::Vector2f(zoomRect.getSize().x / 2, zoomRect.getSize().y / 2)); // Resizing and repositionning the zoom rectangle
+					std::cout << "Resized : " << window.getSize().x << ' ' << window.getSize().y << std::endl;
+				}
+
 				case sf::Event::MouseButtonReleased:  // Zooming
 				{
-					cList.clear();
-					sf::RectangleShape calcArea;
-					
-					calcArea.setSize(sf::Vector2f(zoomRect.getSize().x * zoomRect.getScale().x / lastZoomFactor, zoomRect.getSize().y * zoomRect.getScale().y / lastZoomFactor));
-					calcArea.setOrigin(0, 0);
-					calcArea.setPosition(zoomRect.getPosition().x / lastZoomFactor - calcArea.getSize().x / 2 + lastCalcArea.getPosition().x, zoomRect.getPosition().y / lastZoomFactor - calcArea.getSize().y / 2 + lastCalcArea.getPosition().y);
-					
-					std::cout << lastZoomFactor << ' ' << calcArea.getSize().x << ' ' << calcArea.getSize().y << "\n";
-					std::cout << lastZoomFactor << ' ' << lastCalcArea.getPosition().x << ' ' << calcArea.getPosition().y << "\n";
+					if (event.mouseButton.button == sf::Mouse::Middle)
+					{
+						cList.clear();
+						sf::RectangleShape calcArea;
 
-					calcCList(std::ref(window), &cList, x_acc, y_acc, maxIter, calcArea);
+						calcArea.setSize(sf::Vector2f(zoomRect.getSize().x * zoomRect.getScale().x / lastZoomFactor, zoomRect.getSize().y * zoomRect.getScale().y / lastZoomFactor));
+						calcArea.setOrigin(0, 0);
+						calcArea.setPosition(zoomRect.getPosition().x / lastZoomFactor - calcArea.getSize().x / 2 + lastCalcArea.getPosition().x, zoomRect.getPosition().y / lastZoomFactor - calcArea.getSize().y / 2 + lastCalcArea.getPosition().y);
 
-					std::cout << "\n" << cList[0].position.x << ' ' << cList[0].position.y << "\n";
-					std::cout << "\n" << cList[1].position.x << ' ' << cList[1].position.y << "\n";
-					std::cout << "\n" << cList[2].position.x << ' ' << cList[2].position.y << "\n";
-					std::cout << cList[cList.getVertexCount() - 1].position.x << ' ' << cList[cList.getVertexCount() - 1].position.y << "\n";
+						std::cout << lastZoomFactor << ' ' << calcArea.getSize().x << ' ' << calcArea.getSize().y << "\n";
+						std::cout << lastZoomFactor << ' ' << lastCalcArea.getPosition().x << ' ' << calcArea.getPosition().y << "\n";
+
+						calcCList(std::ref(window), &cList, x_acc, y_acc, maxIter, calcArea);
+
+						std::cout << "\n" << cList[0].position.x << ' ' << cList[0].position.y << "\n";
+						std::cout << "\n" << cList[1].position.x << ' ' << cList[1].position.y << "\n";
+						std::cout << "\n" << cList[2].position.x << ' ' << cList[2].position.y << "\n";
+						std::cout << cList[cList.getVertexCount() - 1].position.x << ' ' << cList[cList.getVertexCount() - 1].position.y << "\n";
+					}
 
 					break;
 				}
@@ -116,7 +136,7 @@ int main()
 
 		///--- Program logics ---///
 		
-		zoomRect.setPosition(sf::Vector2f(sf::Mouse::getPosition(window)));
+		zoomRect.setPosition(sf::Vector2f(window.mapPixelToCoords(sf::Vector2i(sf::Mouse::getPosition(window)))));
 
 		if (!hasCalculated)
 		{
@@ -159,6 +179,7 @@ long double modulus(sf::Vector2f c)
 void seqFunc(sf::Vector2f* seq, sf::Vector2f c)
 {
 	// The function that determines if a point belongs to the Mandelbrot Set
+	//*seq = sf::Vector2f(pow(seq->x, 3) - 3 * seq->x * pow(seq->y, 2) + c.x, 3 * pow(seq->x, 3) * seq->y - pow(seq->y, 3) + c.y);
 	*seq = sf::Vector2f(pow(seq->x, 2) - pow(seq->y, 2) + c.x, 2 * seq->x * seq->y + c.y);
 }
 
